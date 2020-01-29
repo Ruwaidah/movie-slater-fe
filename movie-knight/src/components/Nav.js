824f6d9d314e96f8a757b0a509f9544c8a791764
Krunal Patel <Pkrunal616@gmail.com>
2020-01-28 23:41:08 -0500

    spelling fix and dark mode

--- movie-knight/src/components/Nav.js
+++ movie-knight/src/components/Nav.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import useDarkMode from '../hooks/useDarkMode';
 
 const Nav = props =>{
+    
+    const [DarkMode, setUseDarkMode] = useDarkMode(false)
+
+    const toggleDarkMode = e =>{
+        e.preventDefault()
+        setUseDarkMode(!DarkMode);
+    }
+
     return (
         <div>
 
             <NavLink  to='/' > Home </NavLink>
             <NavLink to='signup' > Sign Up</NavLink>
             <NavLink to='login' > Login</NavLink>
+
+            {
+                DarkMode === true ?
+                (<button onClick={toggleDarkMode} className={DarkMode ? 'dark-button dark-mode' : 'dark-button' } >Light</button>)
+                :
+                (<button onClick={toggleDarkMode} className={DarkMode ? 'dark-button dark-mode' : 'dark-button' } >Dark</button>)   
+            }
 
         </div>
     )
