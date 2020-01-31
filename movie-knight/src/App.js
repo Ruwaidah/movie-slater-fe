f16642b3e392759bfab2578d1a85ba3dd25bc1da
Krunal Patel <Pkrunal616@gmail.com>
2020-01-30 23:51:59 -0500

    Owner signup + login files are made

--- movie-knight/src/App.js
+++ movie-knight/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import OwnerLogin from './components/OwnerLogin';
+import OwnerSignup from './components/OwnerSignup';
 import Navbar from './components/Nav';
 import Dashboard from './components/Dashboard';
 import MovieDetails from './components/MovieDetails';
@@ -23,17 +25,24 @@ function App() {
 
       <Route exact path="/signup" component={Signup}/>
 
+      <Route exact path="/theater-signup" component={OwnerSignup}/>
+      
+      <Route exact path="/theater-login" component={OwnerLogin}/>
+
       <Route exact path="/" component={Dashboard}/>
 
-
       <Route exact path="/details" component={MovieDetails}/>
+
       <Route exact path="/availability" component={Availability}/>
+
       <Route exact path="/checkout" component={Checkout} />
+
       <Route exact path="/event" component={Event} /> 
 
       <PrivateRoute exact path="/ownerdashboard" component={OwnerDashboard}/>
 
       <PrivateRoute exact path="/makeseatingchart" component={MakeSeatingChart}/>
+      
     </div>
   );
 }
---
+++ movie-knight/src/components/OwnerLogin.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import { theaterLogin } from '../actions/index';
+import { connect } from 'react-redux';
+import { withRouter } from "react-router-dom";
+
+const Login = props =>{
+
+        const [loginInfo, setLogininfo] = useState({ email: "", password: "" });
+
+        const handleSubmit = event => {
+          event.preventDefault();
+          props.theaterLogin(loginInfo)
+          props.history.push('/')
+          setLogininfo({ email: "", password: "" });
+        };
+
+        const handleChange = event => {
+          setLogininfo({ ...loginInfo, [event.target.name]: event.target.value });
+        };
+
+    return(
+        <div>
+          <h1>Theater Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <br/>
+          <input
+            required
+            type="text"
+            name="email"
+            placeholder="email"
+            value={loginInfo.email}
+            onChange={handleChange}
+            />
+          <br/>
+          <label>Password</label>
+          <br/>
+          <input
+            required
+            type="password"
+            name="password"
+            placeholder="password"
+            value={loginInfo.password}
+            onChange={handleChange}
+            />
+          <br/>
+          <button type='submit'>Log In</button>
+        </form>
+
+        </div> 
+    )
+
+}
+
+export default connect(null, { theaterLogin })(withRouter(Login))
