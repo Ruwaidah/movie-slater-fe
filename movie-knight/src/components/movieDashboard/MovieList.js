dcda5c2721fe4fa668777b56896be9eb924bf80d
Krunal Patel <Pkrunal616@gmail.com>
2020-02-03 15:49:59 -0500

    zipcode kinda work

--- movie-knight/src/components/movieDashboard/MovieList.js
+++ movie-knight/src/components/movieDashboard/MovieList.js
@@ -7,19 +7,49 @@ import './dashboard.css';
 export default function MovieList(){
   const [movies, setMovies] = useState([])
 
+  const [zipCode, setZipCode] = useState(94080)
+
+  console.log(zipCode);
+
+  const handleChange = e =>{
+    e.preventDefault();
+    setZipCode(e.target.value)
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+  }
+  
+
   useEffect(() => {
-  axios.get(`https://movieknight01.herokuapp.com/api/movies`)
+  axios.get(`https://movieknight01.herokuapp.com/api/movies?zip=${zipCode}`)
     .then(response => {
       console.log(response)
       setMovies(response.data)
     })
-  },[])
+  },[zipCode])
 
   return (
     <div className="movie-list">
-      {movies.map(movie => {
-        return <MovieCard movie={movie} key={movie.tmsId}/>
-      })}
+      <div>
+        <form onSubmit={handleSubmit}>
+
+          <input
+            type='number'
+            name='zipcode'
+            placeholder='zipcode'
+            value={zipCode}
+            onChange={handleChange}
+            />
+            <button type='submit'>🔍</button>
+        </form>
+      </div>
+      
+
+        {movies.map(movie => {
+          return <MovieCard movie={movie} key={movie.tmsId}/>
+        })}
+
     </div>
   )
 }
