04f1b049d8ecaa7b8a5952952ac3f708caf8e7ca
Brad <Brad@BRADSPC>
2020-02-03 14:58:38 -0700

    Adding antd search form

--- movie-knight/src/components/movieDashboard/MovieList.js
+++ movie-knight/src/components/movieDashboard/MovieList.js
@@ -1,25 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieCard from "./MovieCard"
 import './dashboard.css';
+import { Input } from 'antd'
 
 
-export default function MovieList(){
+export default function MovieList() {
   const [movies, setMovies] = useState([])
+  const [searchParam, setSearchParam] = useState([])
+  const { Search } = Input;
 
   useEffect(() => {
-  axios.get(`https://movieknight01.herokuapp.com/api/movies`)
-    .then(response => {
-      console.log(response)
-      setMovies(response.data)
-    })
-  },[])
+    axios.get(`https://movieknight01.herokuapp.com/api/movies?zip=57701`)
+      .then(response => {
+        console.log(response)
+        setMovies(response.data)
+      })
+  }, [])
+
+  const handleChange = event => {
+    setSearchParam({ ...searchParam, [event.target.name]: event.target.value });
+  };
 
   return (
-    <div className="movie-list">
-      {movies.map(movie => {
-        return <MovieCard movie={movie} key={movie.tmsId}/>
-      })}
-    </div>
+    <>
+      <Search
+        placeholder="Search here"
+        onSearch={value => console.log(value)}
+        style={{ width: 200 }}
+        onChange={handleChange}
+      />
+      <div className="movie-list">
+
+        {movies.map(movie => {
+          return <MovieCard movie={movie} key={movie.tmsId} />
+        })}
+      </div>
+    </>
   )
 }
