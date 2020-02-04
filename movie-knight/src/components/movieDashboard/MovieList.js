cd19a3b2f3ba4deb92b866b46284909f7b95b574
Brad <Brad@BRADSPC>
2020-02-04 11:19:34 -0700

    Completed search feature, removed antd, fixed zip code calling backend on change
    Co-authored-by: Krunal1997patel <pkrunal616@gmail.com>

--- movie-knight/src/components/movieDashboard/MovieList.js
+++ movie-knight/src/components/movieDashboard/MovieList.js
@@ -2,27 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieCard from "./MovieCard"
 import './dashboard.css';
-import { Input } from 'antd'
-
 
 export default function MovieList() {
   const [movies, setMovies] = useState([])
-  const [searchParam, setSearchParam] = useState([])
+  const [searchParam, setSearchParam] = useState()
   const [zipCode, setZipCode] = useState(90028)
   const [theatreName, setTheaterName] = useState()
-  const { Search } = Input;
-  
+
   console.log(zipCode);
   console.log(theatreName);
 
-  useEffect(() => {
+  function makeCall() {
     axios.get(`https://movieknight01.herokuapp.com/api/movies?zip=${zipCode}`)
       .then(response => {
         console.log(response)
         setMovies(response.data)
         setTheaterName(response.data[0].showtimes[0].theatre.name)
       })
-  }, [zipCode])
+  }
 
   const handleChange = e => {
     e.preventDefault();
@@ -31,11 +28,23 @@ export default function MovieList() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    makeCall();
   }
 
   const handleChangeSearch = event => {
-    setSearchParam({ ...searchParam, [event.target.name]: event.target.value });
+    console.log(event.target.value)
+    setSearchParam(event.target.value);
   };
+
+  const handleSubmitSearch = event => {
+    //event.preventDefault();
+    console.log("movie state", movies)
+    console.log("target", event.target.value)
+    movies.filter(movie => {
+      console.log("movie", movie)
+      return movie.title.includes(event.target.value);
+    })
+  }
 
   return (
     <>
@@ -54,18 +63,24 @@ export default function MovieList() {
           <button type='submit'>🔍</button>
         </form>
       </div>
-      <Search
+      <form onSubmit={handleSubmitSearch}>
+        <input
+        className="searchForm"
         placeholder="Search here"
-        onSearch={value => console.log(value)}
-        style={{ width: 200 }}
         onChange={handleChangeSearch}
-      />
+        value={searchParam}
+        />
+        <button type="submit">🔍</button>
+      </form>
 
       <div className="movie-list">
 
-        {movies.map(movie => {
-          return <MovieCard movie={movie} key={movie.tmsId} />
-        })}
+        {
+          movies.filter(movie => {
+            return movie.title.toLowerCase().includes(searchParam) || searchParam == null;
+          }).map(movie => {
+            return <MovieCard movie={movie} key={movie.tmsId} />
+          })}
       </div>
     </>
   )
