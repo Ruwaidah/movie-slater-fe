8d90bef9dc28d4ef6a7f3d5177755ca0571294e7
Johnnyr81 <emailjohn81@gmail.com>
2020-02-11 21:52:05 -0600

    fixed sort by filtering

--- movie-knight/src/components/movieDashboard/MovieList.js
+++ movie-knight/src/components/movieDashboard/MovieList.js
@@ -1,59 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import MovieCard from "./MovieCard"
-import './dashboard.scss';
-import { getMovie } from '../../actions/index';
-import { connect } from 'react-redux'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
+import "./dashboard.scss";
+import { getMovie } from "../../actions/index";
+import { connect } from "react-redux";
 function MovieList(props) {
-  const [movies, setMovies] = useState([])
-  const [searchParam, setSearchParam] = useState()
-  const [zipCode, setZipCode] = useState(47712)
+  const [movies, setMovies] = useState([]);
+  const [searchParam, setSearchParam] = useState();
+  const [zipCode, setZipCode] = useState(47712);
+  const [filters, setFilter] = useState({
+    filter: ""
+  });
+  const [values, setValues] = useState({
+    filter: "default"
+  });
+  
   // const [releaseDate, setReleaseDate] = useState({ default: "on", az: "off", recent: "off", soon: "off" })//ReleaseDate
   // const [theatreName, setTheaterName] = useState()
   
-  //NEW CODES//
-  const [values, setValues] = useState({
-    filter: ""
-  });
-   const submit = event => {
+  const submit = event => {
     event.preventDefault();
-    console.log(values);
+    setFilter(values);
+    console.log(filters);
   };
   const change = event => {
     setValues({ filter: event.target.value });
   };
-
+  console.log(filters);
   function makeCall() {
     axios
       .get(`https://movieknight01.herokuapp.com/api/movies?zip=${zipCode}`)
       .then(response => {
-
         console.log(response);
         setMovies(response.data);
-        // setTheaterName(response.data[0].showtimes[0].theatre.name)
       });
   }
-  
-  // console.log(props.fetchingData)
-  // console.log(props.movieList[0])
-
-  useEffect(() =>{
-    makeCall()
-    props.getMovie(zipCode)
-  }, [])
-
-
+  useEffect(() => {
+    makeCall();
+    props.getMovie(zipCode);
+  }, []);
   const handleChange = e => {
     e.preventDefault();
     setZipCode(e.target.value);
   };
-
   const handleSubmit = e => {
     e.preventDefault();
     makeCall();
-    props.getMovie(zipCode)
-  }
+    props.getMovie(zipCode);
+    props.getMovie(zipCode);
+  };
   //ReleaseDate
   // const releaseChange = e => {
   //   e.preventDefault();
@@ -66,47 +61,43 @@ function MovieList(props) {
   //   else
   //     {return null}
   //   toggleMenu();
-    
   // };
-
   const handleChangeSearch = event => {
     console.log(event.target.value);
     setSearchParam(event.target.value);
   };
-
   const toggleMenu = () => {
     document.getElementById("filter").classList.toggle("toggle-menu2");
   };
   return (
     <div className="movielist-component">
-      {/* <h2>{theatreName}</h2> */}
       <br></br>
       <div className="zipsearch">
         <form onSubmit={handleSubmit}>
           <input
+            className="fontAwesome"
             type="number"
             name="zipcode"
-            placeholder="zipcode"
-            value={zipCode}
+            placeholder="&#xf3c5;  Enter zip code to see movies near you"
+            // value={zipCode}
             onChange={handleChange}
           />
-          {/* <button type="submit">🔍</button> */}
         </form>
       </div>
       <div className="searchForm">
         <form>
           <input
-            placeholder=" 🔍 Search here"
+            className="fontAwesome"
+            placeholder="&#xf002;  Search Movies"
             onChange={handleChangeSearch}
             value={searchParam}
           />
         </form>
-
         {/* filter menu */}
         <div onClick={toggleMenu} id="hamburger-menu">
           {/* <img src="./images/menu.png" width="30px" /> */}
           <div className="linediv">
-            Filter   
+            Filter
             <div className="linecon">
               <div className="line1 black"></div>
               <div className="line1 white"></div>
@@ -117,14 +108,13 @@ function MovieList(props) {
             </div>
           </div>
         </div>
-
       </div>
-
       {/* filter menu */}
       <div className="menu-filter" id="filter">
         <div>
-        {/* ReleaseDate */}
-          <div>Sort By :  
+          {/* ReleaseDate */}
+          <div>
+            Sort By :
             {/* <select value={releaseDate} onChange={releaseChange}>
               <option value="default">Default</option>
               <option value="newest">Newest</option>
@@ -136,30 +126,40 @@ function MovieList(props) {
               <input value="soon" name="soon" type="radio" onChange={releaseChange}/>Coming Soon
               <button className="filter-btn" >See Results</button>
             </form>   */}
-            
             {/* NEW CODES */}
             <form onSubmit={submit}>
-              <input type="radio" value="most" name="filter" onChange={change} />
-              most
+              {/* <input
+                type="radio"
+                value="default"
+                name="filter"
+                onChange={change}
+              />
+              Default */}
+              <input
+                type="radio"
+                value="most"
+                name="filter"
+                onChange={change}
+              />
+              Most Recent
               <input type="radio" value="a-z" name="filter" onChange={change} />
-              a-z
-              <input type="radio" value="gener" onChange={change} name="filter" />
-              gener
-              <button>search</button>
+              A-Z
+              <input
+                type="radio"
+                value="soon"
+                onChange={change}
+                name="filter"
+              />
+              Coming Soon
+              <button>See Results</button>
             </form>
           </div>
         </div>
-        <div>
-          Movie Rating
-        </div>
-        <div>
-          Review Rating
-        </div>        
+        <div>Movie Rating</div>
+        <div>Review Rating</div>
       </div>
-
       <div className="movie-list">
-      
-      {movies
+        {movies
           .filter(movie => {
             return (
               movie.title.includes(searchParam) ||
@@ -168,34 +168,34 @@ function MovieList(props) {
             );
           })
           //NEW CODES
-          .sort(function(a, b){
-            if(values.filter === "most")
-              {var dateA = new Date(a.releaseDate), dateB = new Date(b.releaseDate)
-              return dateA-dateB}
-            else if (values.filter === "a-z")
-              {var nameA=a.title.toLowerCase(), nameB=b.title.toLowerCase()
-              if (nameA < nameB) //sort string ascending
-                  return -1 
-              if (nameA > nameB)
-                  return 1
-              return 0}
-            else
-              {return null}
+          .sort(function(a, b) {
+            if (filters.filter === "most") {
+              var dateA = new Date(a.releaseDate),
+                dateB = new Date(b.releaseDate);
+              return dateA - dateB;
+            } else if (filters.filter === "a-z") {
+              var nameA = a.title.toLowerCase(),
+                nameB = b.title.toLowerCase();
+              if (nameA < nameB)
+                //sort string ascending
+                return -1;
+              if (nameA > nameB) return 1;
+              return 0;
+            } else {
+              return null;
+            }
           })
           .map(movie => {
             return <MovieCard movie={movie} key={movie.tmsId} />;
           })}
-
       </div>
     </div>
   );
 }
-
 const mapStateToProps = state => {
   return {
     movieList: state.movieList,
     fetchingData: state.fetchingData
-  }
-}
-
-export default connect(mapStateToProps, { getMovie })(MovieList)
+  };
+};
+export default connect(mapStateToProps, { getMovie })(MovieList);
