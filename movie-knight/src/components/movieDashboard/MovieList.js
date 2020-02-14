ff41bcbf6d9a318759cdc541dfdff4b9d41709a0
Johnnyr81 <emailjohn81@gmail.com>
2020-02-14 14:13:12 -0600

    combine filters

--- movie-knight/src/components/movieDashboard/MovieList.js
+++ movie-knight/src/components/movieDashboard/MovieList.js
@@ -8,89 +8,44 @@ import { connect } from "react-redux";
 function MovieList(props) {
 
   const [movies, setMovies] = useState([])
-  const [maturityRatingsParam, setMaturityRatingsParam] = useState(["R", "PG-13", "PG", "G"])
   const [searchParam, setSearchParam] = useState("")
   const [zipCode, setZipCode] = useState(47712)
-  const [searchParamRating, setSearchParamRating] = useState()
-  const [sortFilter, setSortFilter] = useState({ default: true, az: false, za: false, recent: false, old: false, soon: false })//ReleaseDate
-  const [sortValue, setSortValue] = useState({ default: true, az: false,  za: false, recent: false,  old: false, soon: false })//ReleaseDate
 
-  //Checkbox toggle
-  const [checked, setChecked] = useState({isChecked1: false, isChecked2: false, isChecked3: false, isChecked4: false});
+//FILTER MENU//
+  const [filters, setFilter] = useState({
+    filter: "",
+    rating: "",
+    mature: ""
+  });
+  const [values, setValues] = useState({
+    filter: "",
+    rating: "",
+    mature: ""
+  });
+  console.log(filters)
+  console.log(values)
 
-  const toggleIsChecked1 = (e) => {
-    setChecked({isChecked1: !checked.isChecked1});
-  }
-  const toggleIsChecked2 = (e) => {
-    setChecked({isChecked2: !checked.isChecked2});
-  }
-  const toggleIsChecked3 = (e) => {
-    setChecked({isChecked3: !checked.isChecked3});
-  }
-  const toggleIsChecked4 = (e) => {
-    setChecked({isChecked4: !checked.isChecked4});
-  }
-
-  // const handleButtonClick = (e) => {
-  //   toggleIsChecked();
-  // }
-
-
-  // const [theatreName, setTheaterName] = useState()
-  
-   //ReleaseDate
-  // const submit = event => {
-  //   event.preventDefault();
-  //   setFilter(values);
-  //   console.log(filters);
-  // };
-
-   //ReleaseDate
-  // const change = event => {
-  //   setValues({ filter: event.target.value });
-  // };
-  // console.log(filters);
-
-   //ReleaseDate
-
-  const releaseValue = e => {
-    // e.preventDefault();
-    if(e.target.value === "recent")
-      {return setSortValue({ default: true, az: false, za: false, recent: !sortValue.recent, old: false, soon: false })}
-    else if (e.target.value === "old")
-      {return setSortValue({ default: true, az: false, za: false, recent: false, old: !sortValue.old, soon: false })}  
-    else if (e.target.value === "az")
-      {return setSortValue({ default: true, az: !sortValue.az, za: false, recent: false, old: false, soon: false })}
-    else if (e.target.value === "za")
-      {return setSortValue({ default: true, az: false, za: !sortValue.za, recent: false, old: false, soon: false })}
-    else if (e.target.value === "soon")
-      {return setSortValue({ default: true, az: false, za: false, recent: false, old: false, soon: !sortValue.soon })}
-    else
-      {return null}
-    
-  };
-  console.log(sortValue);
-
-  const releaseSubmit = e => {
-    e.preventDefault();
-    setSortFilter(sortValue);
-    toggleMenu();
-  //   console.log(filters);
-    // if(e.target.value === "recent")
-    //   {return setSortFilter({...sortFilter, recent: "on"})}
-    // else if (e.target.value === "old")
-    //   {return setSortFilter({...sortFilter, old: "on"})}  
-    // else if (e.target.value === "az")
-    //   {return setSortFilter({...sortFilter, az: "on"})}
-    // else if (e.target.value === "za")
-    //   {return setSortFilter({...sortFilter, za: "on"})}  
-    //   else if (e.target.value === "soon")
-    //   {return setSortFilter({...sortFilter, soon: "on"})}
-    // else
-    //   {return null}
+  const submit = event => {
+    event.preventDefault();
+    setFilter(values);
     // toggleMenu();
+    console.log(filters);
   };
-  
+  const change = event => {
+    setValues({ ...values, filter: event.target.value });
+  };
+
+  const changeRating = event => {
+    setValues({ ...values, rating: event.target.value });
+  };
+
+  const changeMature = event => {
+    setValues({ ...values, mature: event.target.value });
+  };
+
+  const toggleMenu = () => {
+    document.getElementById("filter").classList.toggle("toggle-menu2");
+  };
 
   function makeCall() {
     axios
@@ -114,32 +69,11 @@ function MovieList(props) {
     props.getMovie(zipCode);
     props.getMovie(zipCode);
   };
-  
+
   const handleChangeSearch = event => {
     console.log(event.target.value);
     setSearchParam(event.target.value);
   };
-
-  const handleChangeSearchRating = event => {
-    console.log(event.target.value);
-    setSearchParamRating(event.target.value);
-    if(event.target.value !== "reset"){
-      setSearchParam(null);
-    } else {
-      setSearchParam("");
-    }
-  };
-
-  const toggleMenu = () => {
-    document.getElementById("filter").classList.toggle("toggle-menu2");
-  };
-
-  const filterMaturity = rating =>{
-    setMaturityRatingsParam(maturityRatingsParam.map((currRating)=>{
-      return currRating !== rating ? currRating : null;      
-    }))
-  }
-
   return (
     <div className="movielist-component">
       <br></br>
@@ -166,7 +100,7 @@ function MovieList(props) {
         </form>
         {/* filter menu */}
         <div onClick={toggleMenu} id="hamburger-menu">
-          {/* <img src="./images/menu.png" width="30px" /> */}
+         
           <div className="linediv">
             Filter
             <div className="linecon">
@@ -180,127 +114,99 @@ function MovieList(props) {
           </div>
         </div>
       </div>
-      {/* filter menu */}
+      
       <div className="menu-filter" id="filter">
-        <div>
-          Release Date
-        </div>
-        <div className = "movie-rating">
-          Rating
-            <form className="rating" id="ratingSelect" onChange={filterMaturity}>
-              <label>
-                <input type="checkbox" name="stars" id="R" value="R" defaultChecked/>
-              R</label>
-              <label>
-                <input type="checkbox" name="stars" value="PG-13" defaultChecked/>
-              PG-13</label>
-              <label>
-                <input type="checkbox" name="stars" value="PG" defaultChecked/>
-              PG</label>
-              <label>
-                <input type="checkbox" name="stars" value="G" defaultChecked/>
-              G</label>
-            </form>
-          </div>
-        <div>
-          Review Rating
-          <div class = "movie-rating">
-            <form class="rating" id="ratingSelect" onChange={handleChangeSearchRating}>
-              <label>
-                <input type="radio" name="stars" value="reset" />
-                <span>Reset</span>
-              </label>
-              <label>
-                <input type="radio" name="stars" value="1" />
-                <span class="icon-full">★</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-              </label>
-              <label>
-                <input type="radio" name="stars" value="2" />
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-              </label>
-              <label>
-                <input type="radio" name="stars" value="3" />
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon">☆</span>
-                <span class="icon">☆</span>
-              </label>
-              <label>
-                <input type="radio" name="stars" value="4" />
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon">☆</span>
-              </label>
-              <label>
-                <input type="radio" name="stars" value="5" />
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-                <span class="icon-full">★</span>
-              </label>
-            </form>
-          {/* ReleaseDate */}
+        <div> 
+        <div class = "movie-rating">
           <div>
-            Sort By :
+          <form  class="rating" id="ratingSelect" onSubmit={submit}>
+            <div>
+              Release Date
+                </div>
+                <div className = "movie-rating">
+                  Rating
+                    <form className="rating" id="ratingSelect">
+                      <label>
+                        <input type="radio" name="stars" id="R" value="R"  onChange={changeMature} />
+                      R</label>
+                      <label>
+                        <input type="radio" name="stars" value="PG-13"  onChange={changeMature} />
+                      PG-13</label>
+                      <label>
+                        <input type="radio" name="stars" value="PG"  onChange={changeMature}/>
+                      PG</label>
+                    </form>
+                </div>
 
-            {/* <select value={releaseDate} onChange={releaseChange}>
-              <option value="default">Default</option>
-              <option value="newest">Newest</option>
-              <option value="oldest">Oldest</option>
-            </select> */}
+            <label>
+            Review Rating
+            </label>
+              <label>
+                <input type="radio" name="stars" value="1" onChange={changeRating} />
+                <span class="icon-full">★</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="2" onChange={changeRating}/>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="3" onChange={changeRating} />
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon">☆</span>
+                <span class="icon">☆</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="4" onChange={changeRating}/>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon">☆</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="5" onChange={changeRating} />
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+                <span class="icon-full">★</span>
+              </label>  
 
-            <form onSubmit={releaseSubmit}>
-              <input value="recent" name="recent" type="checkbox" onChange={releaseValue} checked={checked.isChecked1} onClick={toggleIsChecked1}/>Most Recent<br></br>
-              <input value="old" name= "old" type="checkbox" onChange={releaseValue} checked={checked.isChecked2} onClick={toggleIsChecked2}/>Oldest<br></br>
-              <input value="az" name="az" type="checkbox" onChange={releaseValue} checked={checked.isChecked3} onClick={toggleIsChecked3}/>A to Z<br></br>
-              <input value="za" name="za" type="checkbox" onChange={releaseValue} checked={checked.isChecked4} onClick={toggleIsChecked4}/>Z to A
-              {/* <input value="soon" name="soon" type="radio" onChange={releaseChange}/>Coming Soon */}
-
-              <div>Movie Rating</div>
-              <div>Review Rating</div>
-              <button className="filter-btn" >See Results</button>
-            </form>  
-
-            {/* NEW CODES */}
-            {/* <form onSubmit={submit}> */}
-              {/* <input
-                type="radio"
-                value="default"
-                name="filter"
-                onChange={change}
-              />
-              Default */}
-              {/* <input
+              Sort By :
+ 
+              <input
                 type="radio"
                 value="most"
                 name="filter"
                 onChange={change}
               />
               Most Recent
-              <input type="radio" value="a-z" name="filter" onChange={change} />
-              A-Z
               <input
                 type="radio"
-                value="soon"
+                value="old"
                 onChange={change}
                 name="filter"
               />
-              Coming Soon
-              <button>See Results</button>
-            </form> */}
+              Oldest
+              <input type="radio" value="az" name="filter" onChange={change} />
+              A-Z
+              <input type="radio" value="za" name="filter" onChange={change} />
+              Z-A
+              
+              <button>search</button>
+            </form>
           </div>
+
         </div>
       </div>
     </div>
@@ -309,31 +215,32 @@ function MovieList(props) {
           .filter(movie => {
             return (
               (movie.title.includes(searchParam) ||
-              movie.title.toLowerCase().includes(searchParam)) ||
-              (movie.ratings && (maturityRatingsParam.includes(movie.ratings.code))) ||
-              (movie.maturityRating[0] && parseInt(movie.maturityRating[0].Value.split("/")[0]) < 2 * parseInt(searchParamRating) + 1 && parseInt(movie.maturityRating[0].Value.split("/")[0]) >= 2 * parseInt(searchParamRating) - 1)
-
+              movie.title.toLowerCase().includes(searchParam)) &&
+              //Mature Rating
+              (movie.ratings[0].code.toLowerCase().includes(filters.mature.toLowerCase()))
+              ||
+              //Review Rating
+              (movie.maturityRating[0] && parseInt(movie.maturityRating[0].Value.split("/")[0]) < 2 * parseInt(filters.rating) + 1 && parseInt(movie.maturityRating[0].Value.split("/")[0]) >= 2 * parseInt(filters.rating) - 1)
             );
           })
-          //NEW CODES
-          .sort(function(a, b) {
-            if (sortFilter.recent === true) {
+             //Sort By
+            .sort(function(a, b) {
+            if (filters.filter === "recent") {
               var dateA = new Date(a.releaseDate),
                 dateB = new Date(b.releaseDate);
               return dateA - dateB;
-            } else if (sortFilter.old === true) {
+            } else if (filters.filter === "old") {
               var dateA = new Date(a.releaseDate),
                 dateB = new Date(b.releaseDate);
               return dateB - dateA;
-            } else if (sortFilter.az === true) {
+            } else if (filters.filter === "az") {
               var nameA = a.title.toLowerCase(),
                 nameB = b.title.toLowerCase();
               if (nameA < nameB)
-                //sort string ascending
                 return -1;
               if (nameA > nameB) return 1;
               return 0;
-            } else if (sortFilter.za === true) {
+            } else if (filters.filter === "za") {
               var nameA = a.title.toLowerCase(),
                 nameB = b.title.toLowerCase();
               if (nameA < nameB)
