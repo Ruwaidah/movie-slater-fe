21f45d0f90f41444993fc6d39a23d015b1f7cdd1
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-14 08:35:01 -0600

    adding movie rate to the movie details page

--- movie-knight/src/components/movieDashboard/MovieCard.js
+++ movie-knight/src/components/movieDashboard/MovieCard.js
@@ -3,7 +3,12 @@ import "./dashboard.scss";
 import { withRouter } from "react-router-dom";
 
 function MovieCard(props) {
+  let path;
   const [active, setActive] = useState(false);
+
+  if (props.movie.ratings)
+    path = `${props.movie.title}&rate=${props.movie.ratings[0].code}`;
+  else path = props.movie.title;
 
   function toggleClass() {
     const currentState = active;
@@ -21,14 +26,14 @@ function MovieCard(props) {
           onClick={toggleClass}
         />
         <p
-          onClick={() => props.history.push(`/details/${props.movie.title}`)}
+          onClick={() => props.history.push(`/details/${path}`)}
           className={active ? "movie-title-enable" : "movie-title-disable"}
         >
           {active ? "View Details" : null}
         </p>
       </div>
       <p
-        onClick={() => props.history.push(`/details/${props.movie.title}`)}
+        onClick={() => props.history.push(`/details/${path}`)}
         className={active ? "movie-title-enable" : "movie-title-disable"}
       >
         {props.movie.title.length > 20
