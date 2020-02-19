28ed5ba63d4ce4bf01213e2fe6cdb620382a7ef4
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-19 14:19:22 -0600

    code climate

--- movie-knight/src/__tests__/App.test.js
+++ movie-knight/src/__tests__/App.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import App from './App';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import OwnerLogin from './components/OwnerLogin';
-import OwnerSignup from './components/OwnerSignup';
+import App from '../App';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
+import OwnerLogin from '../components/OwnerLogin';
+import OwnerSignup from '../components/OwnerSignup';
 import ReactDOM from 'react-dom';
-import Nav from './components/Nav';
-import Dashboard from './components/movieDashboard/Dashboard.js';
-import MovieDetails from './components/MovieDetails';
-import Availability from './components/Availability';
-import Checkout from './components/Checkout';
-import Event from './components/Event';
-import OwnerDashboard from './components/OwnerDashboard';
-import MakeSeatingChart from './components/MakeSeatingChart';
-import PrivateRoute from './components/PrivateRoute';
+import Nav from '../components/Nav';
+import Dashboard from '../components/movieDashboard/Dashboard.js';
+import MovieDetails from '../components/MovieDetails';
+import Availability from '../components/Availability';
+import Checkout from '../components/Checkout';
+import Event from '../components/Event';
+import OwnerDashboard from '../components/OwnerDashboard';
+import MakeSeatingChart from '../components/MakeSeatingChart';
+import PrivateRoute from '../components/PrivateRoute';
 
 // test('App is rendering', () => {
 //   const div = document.createElement('div');
