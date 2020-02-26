c4ebe559f179218a04e2e5777fd37f7acad21e55
Krunal Patel <Pkrunal616@gmail.com>
2020-02-26 17:30:15 -0500

    HashRouting test

--- movie-knight/src/App.js
+++ movie-knight/src/App.js
@@ -25,9 +25,9 @@ function App(props) {
       <Switch>
       <Route path="/profile" render={(props) => <Profile {...props} setShowMenu={setShowMenu} />} />
 
-      <Route exact path="/login" > <Login/> </Route>
+      <Route exact path="/login" component={Login} />
 
-      <Route exact path="/signup" > <Signup/> </Route>
+      <Route exact path="/signup" component={Signup} />
 
       <Route exact path="/" component={Dashboard} />
 
@@ -44,8 +44,6 @@ function App(props) {
       <Route exact path="/tickets" component={Tickets} />
 
       <Route exact path="/showtime" component={Showtime} />
-
-      <Route exact path="*" component={Dashboard}  /> 
       </Switch>
     </div>
   );
